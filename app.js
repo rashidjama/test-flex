650eb3904512d47e6cb87da576338a4e06fe82e0
Rashid Jama <rashidjama5@gmail.com>
2020-01-23 21:46:47 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const celciusInput = document.querySelector("#celcius > input");
+const fahrenheitInput = document.querySelector("#fahrenheit > input");
+
+
+celciusInput.addEventListener("input", function() {
+  const cTem = celciusInput.value;
+  const fTem = (cTem * 9/5) + 32;
+  fahrenheitInput.value = fTem;
+});
+
+let 
